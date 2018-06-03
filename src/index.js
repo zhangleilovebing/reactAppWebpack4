bccc70cae1b7cb1b0253005bee07ae15c187e0a6
张磊 <297752758@qq.com>
2018-06-03 20:45:00 +0800

    x

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Card} from 'antd'
-import "./index.less";
+//import "./index.less";
 class App extends React.Component {
 
     render() {
